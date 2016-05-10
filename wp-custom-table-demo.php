88b236cab2a02707de392d26bac9a4481382cddc
Cameron <jcmaci03@gmail.com>
2016-05-09 21:31:32 -0400

    Add empty plugin file.

---
+++ wp-custom-table-demo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: WP Custom Table Demo
+ * Plugin URI: https://github.com/cambot/wp-transient-demo
+ * Description: Getting to know WordPress' custom table api.
+ * Version: 0.1
+ * Author: Cameron Macintosh
+**/
+
+defined( 'ABSPATH' ) or die( 'No Script kiddies please.' );
+
+class customTableDemo {
+	function __construct() {
+
+	}
+
+}
